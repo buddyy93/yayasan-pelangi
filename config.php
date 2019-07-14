49d35d71caf5bf285f629c0bd253d093c8192109
purashichiyo <purashichiyo@gmail.com>
2019-07-14 16:01:01 +0800

    Ujian Yayasan Element Pelangi

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @var informasi untuk koneksi database
+*/
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'yayasan-pelangi';
+ 
+/** koneksi ke database */
+$db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+ 
+// check koneksi
+if ($db->connect_error) {
+    die('Oops!! Terjadi error : ' . $db->connect_error);
+}
